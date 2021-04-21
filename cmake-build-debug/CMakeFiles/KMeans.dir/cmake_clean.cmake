e2350d36624a7bb4094bb70491329cb5ef5ab8d3
IG <idg7722@gmail.com>
2021-04-21 19:15:56 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/KMeans.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KMeans.dir/main.c.obj"
+  "KMeans.exe"
+  "KMeans.exe.manifest"
+  "KMeans.lib"
+  "KMeans.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/KMeans.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
